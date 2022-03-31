96ff424c0d661b365c9db69efb8282f212a6cd4a
Gabriel Zayas <g-zayas@hotmail.com>
2022-04-01 06:07:11 +0900

    Update Pleiades data

--- javascript/star_clusters/pleiades_data.js
+++ javascript/star_clusters/pleiades_data.js
@@ -13,34 +13,34 @@ const PLEIADES_DATA = {
   "hexagon_2": {
     "initialization_map": [["left", 1]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_1": [["left", 1]],
+      "turn_2": [["up_left", 1]],
+      "turn_3": [["up_right", 1]],
+      "turn_4": [["right", 1]],
+      "turn_5": [["down_right", 1]],
+      "turn_6": [["down_left", 1]],
     }
   },
   "hexagon_3": {
     "initialization_map": [["left", 2]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_1": [["left", 2]],
+      "turn_2": [["up_left", 2]],
+      "turn_3": [["up_right", 2]],
+      "turn_4": [["right", 2]],
+      "turn_5": [["down_right", 2]],
+      "turn_6": [["down_left", 2]],
     }
   },
   "hexagon_4": {
     "initialization_map": [["left", 3]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_1": [["left", 3]],
+      "turn_2": [["up_left", 3]],
+      "turn_3": [["up_right", 3]],
+      "turn_4": [["right", 3]],
+      "turn_5": [["down_right", 3]],
+      "turn_6": [["down_left", 3]],
     }
   }
 }
