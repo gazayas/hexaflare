0373560f761537423eea568ca4b4acdb27c16795
Gabriel Zayas <g-zayas@hotmail.com>
2022-04-05 22:57:56 +0900

    Add Jewel and Butterfly rotation maps

--- javascript/star_clusters/butterfly_data.js
+++ javascript/star_clusters/butterfly_data.js
@@ -13,34 +13,34 @@ const BUTTERFLY_DATA = {
   "hexagon_2": {
     "initialization_map": [["left", 1]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_1": [["down_left", 1]],
+      "turn_2": [["left", 1]],
+      "turn_3": [["up_left", 1]],
+      "turn_4": [["up_right", 1]],
+      "turn_5": [["right", 1]],
+      "turn_6": [["down_right", 1]],
     }
   },
   "hexagon_3": {
     "initialization_map": [["left", 1], ["up_left", 1]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_1": [["down_left", 1], ["left", 1]],
+      "turn_2": [["left", 1], ["up_left", 1]],
+      "turn_3": [["up_left", 1], ["up_right", 1]],
+      "turn_4": [["up_right", 1], ["right", 1]],
+      "turn_5": [["right", 1], ["down_right", 1]],
+      "turn_6": [["down_right", 1], ["down_left", 1]],
     }
   },
   "hexagon_4": {
     "initialization_map": [["up_right", 1]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_1": [["left", 2]],
+      "turn_2": [["up_left", 2]],
+      "turn_3": [["up_right", 2]],
+      "turn_4": [["right", 2]],
+      "turn_5": [["down_right", 2]],
+      "turn_6": [["down_left", 2]],
     }
   }
 }
--- javascript/star_clusters/jewel_data.js
+++ javascript/star_clusters/jewel_data.js
@@ -8,45 +8,45 @@ const JEWEL_DATA = {
   "hexagon_1": {
     "initialization_map": [null],
     "rotation_pattern": {
-      "turn_1": "down_left",
-      "turn_2": "up_left",
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": "up_right",
-      "turn_6": "down_right"
+      "turn_1": null,
+      "turn_2": [["down_left", 1]],
+      "turn_3": [["down_left", 1]],
+      "turn_4": [["left", 1]],
+      "turn_5": [["up_left", 1]],
+      "turn_6": [["up_left", 1]]
     }
   },
   "hexagon_2": {
     "initialization_map": [["down_left", 1]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
+      "turn_1": [["down_left", 1]],
+      "turn_2": [["down_left", 1], ["left", 1]],
+      "turn_3": [["left", 1]],
+      "turn_4": [["up_left", 1]],
+      "turn_5": [["up_right", 1]],
       "turn_6": null,
     }
   },
   "hexagon_3": {
     "initialization_map": [["left", 1]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
+      "turn_1": [["left", 1]],
+      "turn_2": [["left", 1]],
       "turn_3": null,
       "turn_4": null,
       "turn_5": null,
-      "turn_6": null,
+      "turn_6": [["left", 1]],
     }
   },
   "hexagon_4": {
     "initialization_map": [["up_left", 1]],
     "rotation_pattern": {
-      "turn_1": null,
+      "turn_1": [["up_left", 1]],
       "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_3": [["down_right", 1]],
+      "turn_4": [["down_left", 1]],
+      "turn_5": [["left", 1]],
+      "turn_6": [["left", 1], ["up_left", 1]],
     }
   }
 }
