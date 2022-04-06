11867d63d9e57d5a9783a9739589b31406f6a294
Gabriel Zayas <g-zayas@hotmail.com>
2022-04-06 18:52:43 +0900

    Add Lambda data

--- javascript/star_clusters/lambda_data.js
+++ javascript/star_clusters/lambda_data.js
@@ -4,41 +4,41 @@ const LAMBDA_DATA = {
     "rotation_pattern": {
       "turn_1": null,
       "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null
+      "turn_3": [["down_left", 2]],
+      "turn_4": [["left", 2]],
+      "turn_5": [["left", 1], ["up_left", 1]],
+      "turn_6": [["up_left", 1], ["up_right", 1]]
     }
   },
   "hexagon_2": {
     "initialization_map": [["left", 1]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_1": [["left", 1]],
+      "turn_2": [["up_left", 1]],
+      "turn_3": [["down_left", 1]],
+      "turn_4": [["left", 1]],
+      "turn_5": [["left", 1]],
+      "turn_6": [["up_left", 1]],
     }
   },
   "hexagon_3": {
     "initialization_map": [["left", 2]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
+      "turn_1": [["left", 2]],
+      "turn_2": [["up_left", 2]],
       "turn_3": null,
       "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_5": [["down_left", 1]],
+      "turn_6": [["left", 1]],
     }
   },
   "hexagon_4": {
     "initialization_map": [["left", 1], ["down_left", 1]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
+      "turn_1": [["left", 1], ["down_left", 1]],
+      "turn_2": [["up_left", 1], ["left", 1]],
+      "turn_3": [["left", 1]],
+      "turn_4": [["up_left", 1]],
       "turn_5": null,
       "turn_6": null,
     }
