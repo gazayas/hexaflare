7ac7a08ab6139f8d26993a375f767a1266812539
Gabriel Zayas <g-zayas@hotmail.com>
2022-04-06 18:53:13 +0900

    Add Pi data

--- javascript/star_clusters/pi_data.js
+++ javascript/star_clusters/pi_data.js
@@ -4,43 +4,43 @@ const PI_DATA = {
     "rotation_pattern": {
       "turn_1": null,
       "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
+      "turn_3": [["left", 1], ["down_left", 2]],
+      "turn_4": [["up_left", 1], ["left", 2]],
+      "turn_5": [["up_left", 2]],
       "turn_6": null
     }
   },
   "hexagon_2": {
     "initialization_map": [["left", 1]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_1": [["left", 1]],
+      "turn_2": [["up_left", 1]],
+      "turn_3": [["left", 1], ["down_left", 1]],
+      "turn_4": [["up_left", 1], ["left", 1]],
+      "turn_5": [["up_left", 1]],
+      "turn_6": [["down_left", 1]],
     }
   },
   "hexagon_3": {
     "initialization_map": [["left", 1], ["up_left", 1]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
+      "turn_1": [["left", 2]],
+      "turn_2": [["up_left", 2]],
+      "turn_3": [["left", 1]],
+      "turn_4": [["up_left", 1]],
       "turn_5": null,
-      "turn_6": null,
+      "turn_6": [["down_left", 2]],
     }
   },
   "hexagon_4": {
     "initialization_map": [["left", 1], ["up_left", 2]],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
+      "turn_1": [["left", 2], ["up_left", 1]],
+      "turn_2": [["up_left", 2], ["up_right", 1]],
       "turn_3": null,
       "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_5": [["down_left", 1]],
+      "turn_6": [["down_left", 2], ["left", 1]],
     }
   }
 }
