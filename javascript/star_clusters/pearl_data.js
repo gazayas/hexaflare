9a97be9fc687fd9357f97d4d49c602230387e486
Gabriel Zayas <g-zayas@hotmail.com>
2022-04-06 19:13:02 +0900

    Add Pearl data

--- javascript/star_clusters/pearl_data.js
+++ javascript/star_clusters/pearl_data.js
@@ -4,43 +4,43 @@ const PEARL_DATA = {
     "rotation_pattern": {
       "turn_1": null,
       "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null
+      "turn_3": [["down_left", 1]],
+      "turn_4": [["left", 2], ["down_left", 1]],
+      "turn_5": [["up_left", 2], ["left", 1]],
+      "turn_6": [["up_left", 1], ["up_right", 1]],
     }
   },
   "hexagon_2": {
-    "initialization_map": [["down_left", 1]],
+    "initialization_map": [null],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
+      "turn_1": [["left", 1]],
+      "turn_2": [["up_left", 1]],
       "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null,
+      "turn_4": [["left", 1], ["down_left", 1]],
+      "turn_5": [["up_left", 1], ["left", 1]],
+      "turn_6": [["up_left", 1]],
     }
   },
   "hexagon_3": {
-    "initialization_map": [["down_left", 1], ["left", 1]],
+    "initialization_map": [null],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
+      "turn_1": [["left", 1], ["down_left", 1]],
+      "turn_2": [["up_left", 1], ["left", 1]],
+      "turn_3": [["up_left", 1]],
+      "turn_4": [["left", 1]],
+      "turn_5": [["up_left", 1]],
       "turn_6": null,
     }
   },
   "hexagon_4": {
-    "initialization_map": [["down_left", 2], ["left", 1]],
+    "initialization_map": [null],
     "rotation_pattern": {
-      "turn_1": null,
-      "turn_2": null,
-      "turn_3": null,
+      "turn_1": [["down_left", 1], ["left", 2]],
+      "turn_2": [["up_left", 2], ["left", 1]],
+      "turn_3": [["up_left", 1], ["up_right", 1]],
       "turn_4": null,
       "turn_5": null,
-      "turn_6": null,
+      "turn_6": [["down_left", 1]],
     }
   }
 }
