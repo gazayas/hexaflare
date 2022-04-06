eae4795bb490e819a3b257116dcc70317966d2a8
Gabriel Zayas <g-zayas@hotmail.com>
2022-04-06 18:10:55 +0900

    Renew Butterfly rotation positions

--- javascript/star_clusters/butterfly_data.js
+++ javascript/star_clusters/butterfly_data.js
@@ -5,9 +5,9 @@ const BUTTERFLY_DATA = {
       "turn_1": null,
       "turn_2": null,
       "turn_3": null,
-      "turn_4": null,
-      "turn_5": null,
-      "turn_6": null
+      "turn_4": [["left", 2]],
+      "turn_5": [["up_left", 1], ["left", 1]],
+      "turn_6": [["up_left", 1]]
     }
   },
   "hexagon_2": {
@@ -16,9 +16,9 @@ const BUTTERFLY_DATA = {
       "turn_1": [["down_left", 1]],
       "turn_2": [["left", 1]],
       "turn_3": [["up_left", 1]],
-      "turn_4": [["up_right", 1]],
-      "turn_5": [["right", 1]],
-      "turn_6": [["down_right", 1]],
+      "turn_4": [["left", 1], ["up_left", 1]],
+      "turn_5": [["up_left", 1]],
+      "turn_6": null,
     }
   },
   "hexagon_3": {
@@ -27,9 +27,9 @@ const BUTTERFLY_DATA = {
       "turn_1": [["down_left", 1], ["left", 1]],
       "turn_2": [["left", 1], ["up_left", 1]],
       "turn_3": [["up_left", 1], ["up_right", 1]],
-      "turn_4": [["up_right", 1], ["right", 1]],
-      "turn_5": [["right", 1], ["down_right", 1]],
-      "turn_6": [["down_right", 1], ["down_left", 1]],
+      "turn_4": [["up_left", 1]],
+      "turn_5": null,
+      "turn_6": [["down_left", 1]],
     }
   },
   "hexagon_4": {
@@ -38,9 +38,9 @@ const BUTTERFLY_DATA = {
       "turn_1": [["left", 2]],
       "turn_2": [["up_left", 2]],
       "turn_3": [["up_right", 2]],
-      "turn_4": [["right", 2]],
-      "turn_5": [["down_right", 2]],
-      "turn_6": [["down_left", 2]],
+      "turn_4": null,
+      "turn_5": [["down_left", 1]],
+      "turn_6": [["down_left", 1], ["left", 1]],
     }
   }
 }
