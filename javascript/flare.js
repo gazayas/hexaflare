93506046d5db47fab204bf317be5ce23b6d527e2
Gabriel Zayas <g-zayas@hotmail.com>
2022-04-30 22:05:21 +0900

    Minor tweaks in flare file

--- javascript/flare.js
+++ javascript/flare.js
@@ -1,23 +1,14 @@
 function processStarsAfterDrop() {
   var flare_star_rings = document.getElementsByClassName("ring")
 
-  if(fullRingExists(flare_star_rings)) {
-    // Check which rings are full, flare the rings, gravitate the stars, and flare until no full rings
-
-    // Go through each ring and destroy its stars
-    // This should all happen in one go, so make sure
-    // any animation added goes in the right place.
+  while(fullRingExists(flare_star_rings)) {
     for (var i = 0; i < flare_star_rings.length; i++) {
       if(ringIsFull(flare_star_rings[i])){
         flare(flare_star_rings[i])
-        if(parseInt(flare_star_rings[i].dataset["level"]) == 1) {
-          flareCore()
-        }
+        if(parseInt(flare_star_rings[i].dataset["level"]) == 1) { flareTheCore() }
       }
     }
-    // TODO: Flare the core if the first ring is full
-
-    // Get all the stars, gravitate them toward the center
+    // Gravitate each individual star from ring level 1 and up
   }
 
   // stars in corona ? end game : generateStarCluster
@@ -49,8 +40,7 @@ function flare(flare_star_ring) {
   }
 }
 
-function flareCore() {
-  console.log("here");
+function flareTheCore() {
   var core_element = document.getElementsByClassName("core")[0]
   core_element.children[0].dataset["full"] = false // children[0] is background_hexagon
 
